4aeb4ea2cfb4591930e7e7ce02faabc5e537a7ae
Ayush Singh <ayush440896@gmail.com>
2025-05-08 12:30:39 +0530

    frontend run build setup

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   }, []);
 
   async function reviewCode() {
-    const response = await axios.post("http://localhost:3000/ai/get-review", {code});
+    const response = await axios.post("https://code-reviewer-backend-qfjt.onrender.com", {code});
 
     setreview(response.data);
     //  console.log(response.data)
